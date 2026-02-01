11b6a1f93b9db7761f2470516a197c2db677aa1f
Ashutosh Ayush <jbnashu297@gmail.com>
2026-02-01 15:55:08 +0530

    Add deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   // Setting base to './' ensures assets are loaded correctly on GitHub Pages subfolders
   base: './',
@@ -12,6 +11,11 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: false
+    sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
   }
 });
